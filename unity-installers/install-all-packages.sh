359ad5369202fabb019b27247c81b8df06e5bb1a
simonleary-umass-edu <simonleary@umass.edu>
2022-10-03 03:46:54 +0000

    lots more stuff

--- unity-installers/install-all-packages.sh
+++ unity-installers/install-all-packages.sh
@@ -8,8 +8,20 @@ done
 
 IFS=$'\n'
 packages=$(<state/packagelist.txt)
-
 for package in $packages; do
+# bash seems to remove empty lines on its own
+#    if [[ "$package" == '' ]]; then
+#        echo "disregarding '$package'..."
+#        continue
+#    fi
+    if [[ $package =~ ^[[:space:]]+$ ]]; then
+        echo "disregarding '$package'..."
+        continue
+    fi
+    if [[ $package =~ ^[[:space:]]*\#.*$ ]]; then
+        echo "disregarding '$package'..."
+        continue
+    fi
     # if $USER_ARCH is defined
     if [ ! -z ${USER_ARCH+x} ]; then
         ./install-package.sh -y -a $USER_ARCH $package
