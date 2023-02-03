796318b62fefc340caea09822d6c056a378fd178
simon <simon.leary42@gmail.com>
2023-02-03 10:10:15 -0500

    update spider cache script for new lmod

--- unity-installers/update-spider-cache.sh
+++ unity-installers/update-spider-cache.sh
@@ -2,7 +2,5 @@
 # the microarch modules allow Lmod to crawl through the spack modules for each microarch
 # when we make the hierarchy change, we just have to change the microarch modules
 export MODULEPATH="/modules/modulefiles"
-
-source /modules/lmod/lmod/lmod/libexec/update_lmod_system_cache_files -d /modules/lmod/cache $MODULEPATH
-
-echo "this does not update spider cache for power9 nodes, their cache is somewhere else"
+# -D debug
+source /modules/lmod/lmod/lmod/libexec/update_lmod_system_cache_files -D $MODULEPATH
