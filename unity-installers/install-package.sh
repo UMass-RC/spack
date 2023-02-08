146de217e2535509266d597a577bc00fa6dd975d
simonleary-umass-edu <simonleary@umass.edu>
2023-02-07 22:58:38 -0500

    fix bug in install-package copying spack stage

--- unity-installers/install-package.sh
+++ unity-installers/install-package.sh
@@ -124,14 +124,14 @@ cat <<- BATCH_SCRIPT > $BATCH_SCRIPT_PATH
 	spack $DEBUG install \$SPACK_INSTALL_ARGS
 	if [ ! \$? -eq 0 ]; then
 		# copy spack-build-out and spack-build-env in case we need to make a github issue
-		stage_dir=/scratch/\$(hostname)/\$SLURM_JOB_ID/\$(whoami)/spack-stage/*
-		ARGS_LIST=(\$SPACK_INSTALL_ARGS)
-		# 2nd to last argument is the package name, last argument is the target architecture
-		stage_copy_dir=$PREFIX/logs/failed-builds/\$(date +%s)-\${ARGS_LIST[-2]}-\${ARGS_LIST[-1]}
+		stage_dir=/scratch/\$(hostname)/\$SLURM_JOB_ID/\$(whoami)/spack-stage/
 		if [ -d \$stage_dir ]; then
+			ARGS_LIST=(\$SPACK_INSTALL_ARGS)
+			# 2nd to last argument is the package name, last argument is the target architecture
+			stage_copy_dir=$PREFIX/logs/failed-builds/\$(date +%s)-\${ARGS_LIST[-2]}-\${ARGS_LIST[-1]}
 			mkdir -p \$stage_copy_dir
-			cp \$stage_dir/spack-build-out.txt \$stage_copy_dir/
-			cp \$stage_dir/spack-build-env.txt \$stage_copy_dir/
+			cp \$stage_dir/*/spack-build-out.txt \$stage_copy_dir/
+			cp \$stage_dir/*/spack-build-env.txt \$stage_copy_dir/
 			echo "spack logs copied to \$stage_copy_dir/"
 		else
 			echo "spack stage directory \$stage_dir doesn't exist!"
